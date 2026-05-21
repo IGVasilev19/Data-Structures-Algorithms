557fb68071bdfc8dac50935eacf1fe5ce2e4151f
IGVasilev19 <ivelin.vasilev@student.fontys.nl>
2026-05-21 10:54:03 +0200

    Add Week1 completed assignments

--- Exercises/D&A2/Week1/Week1.py
+++ Exercises/D&A2/Week1/Week1.py
@@ -21,5 +21,92 @@ def all_combinations(vertices):
 
 #Vertex Cover check
 
+def getAllEdges(matrix):
+    allEdges = []
+
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            if matrix[i][j] == 1:
+                allEdges.append([i+1]+[j+1])
+
+    return allEdges
+
 def checkCover(matrix, vertices):
+    allPairs = getAllEdges(matrix)
+
+    for pair in allPairs[:]:
+        if any(vertex in vertices for vertex in pair):
+            allPairs.remove(pair)
+
+    if len(allPairs) == 0:
+        return True
+
+    return False
+
+
+# matrix = [
+#     [0, 1, 1, 0, 0, 0, 0, 0, 0],
+#     [1, 0, 1, 0, 0, 1, 0, 0, 0],
+#     [1, 1, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 1, 1, 0, 0, 1, 0, 0],
+#     [0, 1, 0, 0, 0, 0, 1, 0, 0],
+#     [0, 0, 0, 0, 1, 1, 0, 1, 1],
+#     [0, 0, 0, 0, 0, 0, 1, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 1, 0, 0],
+# ]
+
+# print(checkCover(matrix,[1,2,3]))
+
+
+#Brute-force Vertex Cover
+def findCover(matrix, vertices):
+    allCombinations = all_combinations(vertices)
+    bestCombination = [0] * 1000
+
+    for combination in allCombinations:
+        if checkCover(matrix, combination) and len(combination) < len(bestCombination):
+            bestCombination = combination
+            
+    return bestCombination
+
+# matrix = [
+#     [0, 1, 1, 0, 0, 0, 0, 0, 0],
+#     [1, 0, 1, 0, 0, 1, 0, 0, 0],
+#     [1, 1, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 1, 1, 0, 0, 1, 0, 0],
+#     [0, 1, 0, 0, 0, 0, 1, 0, 0],
+#     [0, 0, 0, 0, 1, 1, 0, 1, 1],
+#     [0, 0, 0, 0, 0, 0, 1, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 1, 0, 0],
+# ]
+
+# print(findCover(matrix,[1,2,3,4,5,6,7,8,9]))
+
+
+def getAllPossiblePairs(vertices):
+    pairs = []
     
+    for vertice in vertices:
+        for i in range(0, len(vertices)):
+            if vertice != vertices[i]:
+                pairs.append([vertice,vertices[i]])
+
+    return pairs
+
+import random
+
+def should_do(p):
+    return random.random() <= p
+
+def graphGenerator(vertices, p):
+    pairs = getAllPossiblePairs(vertices)
+    matrix = [[0] * len(vertices) for _ in range(len(vertices))]
+
+    for pair in pairs:
+        if should_do(p):
+            matrix[pair[0]-1][pair[1]-1] = 1
+
+    return matrix
+
